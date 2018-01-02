b233ca0ad60905729927dc5746215cda2456389b
Maria Dima <dima.maria@gmail.com>
2018-01-02 18:23:54 +0200

    Support Unicode for typeString() in Windows.
    * Removed the surrogates implementation part.
      The reason for this:
       - The toggleUnicode method(former toggleUnicodeKey) was called only by typeString() that supports only UTF-8 at the moment.
       - There were some warnings both on mac and windows. Since this part of the code will never be activated, I removed it.
    * Refactored a little bit to make typeStringDelayed() functionality identical to typeString().
    * Don't type the word "undefined" if typeString() or typeStringDelayed() is called with no arguments.
    * Tests added to cover all changes.

--- test/keyboard.js
+++ test/keyboard.js
@@ -5,7 +5,8 @@ var os = require('os');
 
 describe('Keyboard', () => {
   it('Tap a key.', function() {
-    expect(() => robot.keyTap('a')).not.toThrow();
+		expect(() => robot.keyTap('a')).not.toThrow();
+		expect(() => robot.keyTap('control')).not.toThrow();
     expect(() => robot.keyTap('a', 'control')).not.toThrow();
     expect(() => robot.keyTap()).toThrowError(/Invalid number/);
   });
@@ -41,11 +42,44 @@ describe('Keyboard', () => {
 
 	it('Tap a UTF32 character.', function()
   {
-		expect(() => robot.utf32Tap("r".charCodeAt(0))).not.toThrow();
-		expect(() => robot.utf32Tap("ά".charCodeAt(0))).not.toThrow();
-		expect(() => robot.utf32Tap("ö".charCodeAt(0))).not.toThrow();
-		expect(() => robot.utf32Tap("ち".charCodeAt(0))).not.toThrow();
-		expect(() => robot.utf32Tap("嗨".charCodeAt(0))).not.toThrow();
+		expect(() => robot.utf32Tap('r'.charCodeAt(0))).not.toThrow();
+		expect(() => robot.utf32Tap('ά'.charCodeAt(0))).not.toThrow();
+		expect(() => robot.utf32Tap('ö'.charCodeAt(0))).not.toThrow();
+		expect(() => robot.utf32Tap('ち'.charCodeAt(0))).not.toThrow();
+		expect(() => robot.utf32Tap('嗨'.charCodeAt(0))).not.toThrow();
+		expect(() => robot.utf32Tap('ఝ'.charCodeAt(0))).not.toThrow();
 		expect(() => robot.utf32Tap()).toThrowError(/Invalid character typed./);
+	});
+
+	it('Test Key Toggle.', function()
+  {
+		expect(() => robot.keyToggle('a', 'down')).not.toThrow();
+		expect(() => robot.keyToggle('a', 'up')).not.toThrow();
+		expect(() => robot.keyToggle('ά', 'down')).toThrowError(/Invalid key code specified./);
+		expect(() => robot.keyToggle('ά', 'up')).toThrowError(/Invalid key code specified./);
+		expect(() => robot.keyToggle('嗨', 'down')).toThrowError(/Invalid key code specified./);
+		expect(() => robot.keyToggle("嗨", "up")).toThrowError(/Invalid key code specified./);
+	});
+
+	it('Type Ctrl+Shift+RightArrow.', function()
+  {
+		var modifiers = [];
+  	modifiers.push('shift');
+		modifiers.push('control');
+
+		expect(() => robot.keyToggle("right", "down", modifiers)).not.toThrow();
+		expect(() => robot.keyToggle("right", "up", modifiers)).not.toThrow();
+	});
+
+	it('Type a string.', function()
+  {
+		expect(() => robot.typeString("Typed rάöち嗨ఝ 1")).not.toThrow();
+		expect(() => robot.typeString()).toThrowError(/Invalid number of arguments./);
+	});
+
+	it('Type a string with delay.', function()
+  {
+		expect(() => robot.typeStringDelayed("Typed rάöち嗨ఝ with delay 1", 600)).not.toThrow();
+		expect(() => robot.typeStringDelayed()).toThrowError(/Invalid number of arguments./);
 	});
 });
